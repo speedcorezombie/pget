2bd960fc367eb19fc92d5209a776677a85e37689
speedcorezombie <speedcorezombie@gmail.com>
2012-04-08 23:47:30 +0400

    header add

---
+++ pget.h
@@ -0,0 +1,16 @@
+#include <pcap/pcap.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql.h>
+
+#define MAX_HTTP_SIZE 2048               // Max allowed HTTP packet size
+#define MAX_FIELD_SIZE 128               // Max allowed field size in HTTP header
+#define MAX_REQ_SIZE 256                 // Max allowed request string size
+#define ETH_HDR_SIZE 14                  // Ethernet header size
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+
+MYSQL* mysql_conn();
