76d74b31d44c5cc7aa8d587839b398a03cbf6d53
speedcorezombie <speedcorezombie@gmail.com>
2012-04-12 22:55:37 +0400

    analizer add

---
+++ analyzer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IP_HIT_LIMIT=20
+HOST_HIT_LIMIT=50
+RANGE=120
+TTL=180
+
+while [ 1 ]; do
+	TIME=`date +%s`;
+	mysql -e "INSERT INTO top_ips (timestamp, ip_src_addr, hits_per_sec) SELECT $TIME, ip_src_addr, CAST(COUNT(ip_src_addr)/$RANGE AS UNSIGNED) AS hits_per_sec FROM headers WHERE timestamp BETWEEN $TIME - $RANGE AND $TIME GROUP BY ip_src_addr HAVING hits_per_sec >= $IP_HIT_LIMIT  ORDER BY hits_per_sec;"
+	mysql -e "INSERT INTO top_hosts (timestamp, http_host, hits_per_sec) SELECT $TIME, http_host, CAST(COUNT(ip_src_addr)/$RANGE AS UNSIGNED) AS hits_per_sec FROM headers WHERE timestamp BETWEEN $TIME - 60 AND $TIME GROUP BY http_host HAVING hits_per_sec >= $HOST_HIT_LIMIT ORDER BY hits_per_sec;"
+	IPLIST=`mysql -sN -e "SELECT INET_NTOA(ip_src_addr) FROM top_ips WHERE timestamp = '$TIME';"`
+	for a in `echo $IPLIST`; do
+		echo "/sbin/iptables -I bad-http 1 -s $a -j DROP"
+	done;
+#	HOSTLIST=`mysql -sN -e "SELECT http_host FROM top_hosts WHERE timestamp = '$TIME';"`	
+	mysql -e "DELETE FROM headers WHERE timestamp < $TIME - $TTL;"
+	sleep 60;
+done;
